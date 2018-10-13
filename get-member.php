4a759311246585f19c1b5ed4e329c2285ccbf502
nouphet <nouphet.ratanavong@gmail.com>
2018-10-13 19:05:09 +0900

    Fix 戻り値の型指定、functionの位置を移動、コメントを修正 #3

--- get-member.php
+++ get-member.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', "Off");
 require_once './phpQuery-onefile.php';
 
-function print_list($list) {
+function print_list($list): void  {
     $lines = explode("\n", $list);
     foreach($lines as $line) {
         $line = trim($line);
@@ -14,11 +14,21 @@ function print_list($list) {
     echo "\n";
 }
 
+// 参加車のリストを取得して表示
+function print_members_of_participants($doc): void {
+    for ($i = 3; $i <= 5; $i++) {
+        echo '## ' . $doc["#main > .applicant_area > div:nth-child($i) > table > thead > tr > th > span.label_ptype_name"]->text();
+        echo "\n";
+        $list = $doc["#main > div.applicant_area > div:nth-child($i)"]->find(".display_name")->text();
+        print_list($list);
+    }
+}
+
 // 引数があるかチェック
 if ($argv[1] == null) {
     echo 'Missing $url Parameter.' . "\n";
     echo 'Please set Connpass URL.' . "\n";
-    echo 'ex) php get-member.php https://yyphp.connpass.com/event/103258/' . "\n";
+    echo 'ex.) php get-member.php https://yyphp.connpass.com/event/103258/' . "\n";
     exit;
 }
 
@@ -33,21 +43,12 @@ if ($html == false) {
     exit;
 }
 
-// 取得したい情報を記述
+// 取得したい情報の全体を取得
 $doc = phpQuery::newDocument($html);
 
 // タイトル
 echo '# ' . $doc["head"]["title"]->text() . "\n";
 echo  '①PHP歴、②話したいこと/聞きたいこと、③簡単な自己紹介（オプション）' . "\n";
 
-function print_member_of_participants($doc) {
-    for ($i = 3; $i <= 5; $i++) {
-        echo '## ' . $doc["#main > .applicant_area > div:nth-child($i) > table > thead > tr > th > span.label_ptype_name"]->text();
-        echo "\n";
-        $list = $doc["#main > div.applicant_area > div:nth-child($i)"]->find(".display_name")->text();
-        print_list($list);
-    }
-}
-
 /* main */
-print_member_of_participants($doc);
+print_members_of_participants($doc);
